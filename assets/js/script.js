2aae8b7cc701bdf248ef2066170e7908e1ecab5f
Nic Carrig <cleamcarrig90@hotmail.com>
2022-04-27 17:36:00 -0700

    touch ups

--- assets/js/script.js
+++ assets/js/script.js
@@ -136,18 +136,20 @@ $(".card .list-group").sortable({
   scroll: false,
   tolerance: "pointer",
   helper: "clone",
-  // activate: function(event){
-  //   console.log("activate", this)
-  // },
-  // deactivate: function(event){
-  //   console.log("deactivate", this)
-  // },
-  // over: function(event){
-  //   console.log("over", event.target);
-  // },
-  // out: function(event){
-  //   console.log("out", event.target);
-  // },
+  activate: function(event){
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
+  },
+  deactivate: function(event){
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
+  },
+  over: function(event){
+    $(event.target).addClass("dropover-active");
+  },
+  out: function(event){
+    $(event.target).removeClass("dropover-active");
+  },
   update: function(event){
     var tempArr = [];
 
@@ -177,14 +179,14 @@ $("#trash").droppable({
   accept: ".card .list-group-item",
   tolerance: "touch",
   drop: function(event, ui){
-    console.log("drop");
     ui.draggable.remove();
+    $(".bottom-trash").removeClass("bottom-trash-active");
   },
   over: function(event, ui){
-    console.log("over");
+    $(".bottom-trash").addClass("bottom-trash-active");
   },
   out: function(event, ui){
-    console.log("out");
+    $(".bottom-trash").removeClass("bottom-trash-active");
   }
 });
 
@@ -204,7 +206,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -233,6 +235,12 @@ $("#remove-tasks").on("click", function() {
   }
   saveTasks();
 });
+
+setInterval(function(){
+  $(".card .list-group-item").each(function(index, el){
+    auditTask(el);
+  });
+}, (1000 * 60) * 30);
 
 
 // load tasks for the first time
